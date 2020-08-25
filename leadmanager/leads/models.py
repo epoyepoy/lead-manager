4e662d114025c393246b8ed7a2a636efe6ea2aad
Eugene <eugenepaul83@gmail.com>
2020-08-25 20:47:22 +0400

    Modified models.

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
-    message = models.CharField(max_length=500, blank=True)
+    tel = models.CharField(max_length=50, blank=True)
+    company = models.CharField(max_length=100, blank=True)
+    position = models.CharField(max_length=50, blank=True)
+    remarks = models.CharField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
