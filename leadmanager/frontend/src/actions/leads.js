8929c79b24ff481da745f9d2a64ba7ff888cf53f
epoyepoy <eugenepaul83@gmail.com>
2020-08-27 16:18:44 +0400

    Created an alerts component to handle messaging.

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
+import { createMessage } from './messages';
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
 
 // GET LEADS
-export const getLeads = () => (dispath) => {
+export const getLeads = () => (dispatch) => {
   axios
     .get('api/leads/')
     .then((res) => {
-      dispath({
+      dispatch({
         type: GET_LEADS,
         payload: res.data,
       });
@@ -16,11 +17,12 @@ export const getLeads = () => (dispath) => {
 };
 
 // DELETE LEADS
-export const deleteLead = (id) => (dispath) => {
+export const deleteLead = (id) => (dispatch) => {
   axios
     .delete(`api/leads/${id}/`)
     .then((res) => {
-      dispath({
+      dispatch(createMessage({ deleteLead: 'Lead Deleted' }));
+      dispatch({
         type: DELETE_LEAD,
         payload: id,
       });
@@ -33,10 +35,20 @@ export const addLead = (lead) => (dispatch) => {
   axios
     .post('/api/leads/', lead)
     .then((res) => {
+      dispatch(createMessage({ addLead: 'Lead Added' }));
       dispatch({
         type: ADD_LEAD,
         payload: res.data,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const errors = {
+        msg: err.response.data,
+        status: err.response.status,
+      };
+      dispatch({
+        type: GET_ERRORS,
+        payload: errors,
+      });
+    });
 };
