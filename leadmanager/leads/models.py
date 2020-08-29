637d601c6a172b1c3341f092601576cf906a8a50
epoyepoy <eugenepaul83@gmail.com>
2020-08-29 15:56:10 +0400

    Implemented the backend auth using knox token authentication

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Lead(models.Model):
@@ -8,4 +9,6 @@ class Lead(models.Model):
     company = models.CharField(max_length=100, blank=True)
     position = models.CharField(max_length=50, blank=True)
     remarks = models.CharField(max_length=500, blank=True)
+    owner = models.ForeignKey(
+        User, related_name="leads", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
